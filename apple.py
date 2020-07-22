e8082f908748f39484d80e262db06a8d9f243add
Michael LeCompte <michaelslec98@gmail.com>
2020-07-22 02:26:30 -0700

    Moved globals into config class

--- apple.py
+++ apple.py
@@ -1,11 +1,11 @@
 import random
-from game import Game
+from config import Config
 
 class Apple():
     def __init__(self):
         self.setNewLocation()
     
     def setNewLocation(self):
-        self.x = random.randint(0, Game.CELLWIDTH - 1)
-        self.y = random.randint(0, Game.CELLHEIGHT - 1)
+        self.x = random.randint(0, Config.CELLWIDTH - 1)
+        self.y = random.randint(0, Config.CELLHEIGHT - 1)
         
--- game.py
+++ game.py
@@ -1,38 +1,25 @@
+from config import Config
+from snake import Snake
+from apple import Apple
 import pygame
 
 
 class Game():
-    FPS = 15
-    WINDOW_WIDTH = 640
-    WINDOW_HEIGHT = 480
-    CELLSIZE = 20
-    assert WINDOW_WIDTH % CELLSIZE == 0, "Window width must be a multiple of cell size."
-    assert WINDOW_HEIGHT % CELLSIZE == 0, "Window height must be a multiple of cell size."
-    CELLWIDTH = int(WINDOW_WIDTH / CELLSIZE)
-    CELLHEIGHT = int(WINDOW_HEIGHT / CELLSIZE)
-
-    # Colors
-    WHITE = (255, 255, 255)
-    BLACK = (0,   0,   0)
-    RED = (255,   0,   0)
-    GREEN = (0, 255,   0)
-    DARKGREEN = (0, 155,   0)
-    DARKGRAY = (40,  40,  40)
-    BG_COLOR = BLACK
-
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode(
-            (self.WINDOW_WIDTH, self.WINDOW_HEIGHT))
+            (Config.WINDOW_WIDTH, Config.WINDOW_HEIGHT))
         self.clock = pygame.time.Clock()
         self.BASICFONT = pygame.font.Font('freesansbold.ttf', 18)
         pygame.display.set_caption('Wormy')
+        self.apple = Apple()
+        self.snake = Snake()
 
     def showStartScreen(self):
         titleFont = pygame.font.Font('freesansbold.ttf', 100)
         titleSurf1 = titleFont.render(
-            'Wormy!', True, self.WHITE, self.DARKGREEN)
-        titleSurf2 = titleFont.render('Wormy!', True, self.GREEN)
+            'Wormy!', True, Config.WHITE, Config.DARKGREEN)
+        titleSurf2 = titleFont.render('Wormy!', True, Config.GREEN)
         degrees1 = 0
         degrees2 = 0
 
@@ -40,36 +27,40 @@ class Game():
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
                     self.run()
-            self.screen.fill(self.BG_COLOR)
+            self.screen.fill(Config.BG_COLOR)
             rotatedSurf1 = pygame.transform.rotate(titleSurf1, degrees1)
             rotatedRect1 = rotatedSurf1.get_rect()
-            rotatedRect1.center = (self.WINDOW_WIDTH / 2,
-                                   self.WINDOW_HEIGHT / 2)
+            rotatedRect1.center = (Config.WINDOW_WIDTH / 2,
+                                   Config.WINDOW_HEIGHT / 2)
             self.screen.blit(rotatedSurf1, rotatedRect1)
 
             rotatedSurf2 = pygame.transform.rotate(titleSurf2, degrees2)
             rotatedRect2 = rotatedSurf2.get_rect()
-            rotatedRect2.center = (self.WINDOW_WIDTH / 2,
-                                   self.WINDOW_HEIGHT / 2)
+            rotatedRect2.center = (Config.WINDOW_WIDTH / 2,
+                                   Config.WINDOW_HEIGHT / 2)
             self.screen.blit(rotatedSurf2, rotatedRect2)
 
             self.drawPressKeyMsg()
 
             pygame.display.update()
-            self.clock.tick(self.FPS)
+            self.clock.tick(Config.FPS)
             degrees1 += 3  # rotate by 3 degrees each frame
             degrees2 += 7  # rotate by 7 degrees each frame
 
     def drawPressKeyMsg(self):
         pressKeySurf = self.BASICFONT.render(
-            'Press a key to play.', True, self.DARKGRAY)
+            'Press a key to play.', True, Config.DARKGRAY)
         pressKeyRect = pressKeySurf.get_rect()
-        pressKeyRect.topleft = (self.WINDOW_WIDTH - 200,
-                                self.WINDOW_HEIGHT - 30)
+        pressKeyRect.topleft = (Config.WINDOW_WIDTH - 200,
+                                Config.WINDOW_HEIGHT - 30)
         self.screen.blit(pressKeySurf, pressKeyRect)
 
     def handleKeyEvents(self, event):
         print(event)
+
+    def draw(self):
+        print(self.apple.x)
+        print(self.apple.y)
 
     def run(self):
         while True:
@@ -79,5 +70,5 @@ class Game():
                 elif event.type == pygame.KEYDOWN:
                     self.handleKeyEvents(event)
 
-            self.screen.fill(self.BG_COLOR)
+            self.draw()
             pygame.display.flip()
