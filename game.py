a221acd1e5845d6475334cca3e786aee440cf807
Michael LeCompte <michaelslec98@gmail.com>
2020-07-22 02:56:57 -0700

    Added drawWorm()

--- game.py
+++ game.py
@@ -57,13 +57,13 @@ class Game():
 
     def handleKeyEvents(self, event):
         if (event.key == pygame.K_LEFT or event.key == pygame.K_a) and self.snake.direction != self.snake.RIGHT:
-            direction = self.snake.LEFT
+            self.snake.direction = self.snake.LEFT
         elif (event.key == pygame.K_RIGHT or event.key == pygame.K_d) and self.snake.direction != self.snake.LEFT:
-            direction = self.snake.RIGHT
+            self.snake.direction = self.snake.RIGHT
         elif (event.key == pygame.K_UP or event.key == pygame.K_w) and self.snake.direction != self.snake.DOWN:
-            direction = self.snake.UP
+            self.snake.direction = self.snake.UP
         elif (event.key == pygame.K_DOWN or event.key == pygame.K_s) and self.snake.direction != self.snake.UP:
-            direction = self.snake.DOWN
+            self.snake.direction = self.snake.DOWN
         elif event.key == pygame.K_ESCAPE:
             pygame.quit()
 
@@ -76,11 +76,23 @@ class Game():
             pygame.draw.line(self.screen, Config.DARKGRAY,
                              (0, y), (Config.WINDOW_WIDTH, y))
 
+    def drawWorm(self):
+        for coord in self.snake.wormCoords:
+            x = coord['x'] * Config.CELLSIZE
+            y = coord['y'] * Config.CELLSIZE
+            wormSegmentRect = pygame.Rect(
+                x, y, Config.CELLSIZE, Config.CELLSIZE)
+            pygame.draw.rect(self.screen, Config.DARKGREEN, wormSegmentRect)
+            wormInnerSegmentRect = pygame.Rect(
+                x + 4, y + 4, Config.CELLSIZE - 8, Config.CELLSIZE - 8)
+            pygame.draw.rect(self.screen, Config.GREEN, wormInnerSegmentRect)
+
     def draw(self):
         self.screen.fill(Config.BG_COLOR)
         self.drawGrid()
+        self.drawWorm()
         pygame.display.update()
-        self.clock.tick()
+        self.clock.tick(Config.FPS)
 
     def run(self):
         while True:
